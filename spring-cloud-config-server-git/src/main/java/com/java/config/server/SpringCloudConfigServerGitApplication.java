819469811d82f316dbbac7a46060e5db58e0da33
k307121 <sjainapur@zensar.com>
2019-05-27 16:29:01 +0530

    Modified

--- spring-cloud-config-server-git/src/main/java/com/java/config/server/SpringCloudConfigServerGitApplication.java
+++ spring-cloud-config-server-git/src/main/java/com/java/config/server/SpringCloudConfigServerGitApplication.java
@@ -11,5 +11,4 @@ public class SpringCloudConfigServerGitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServerGitApplication.class, args);
 	}
-
 }
